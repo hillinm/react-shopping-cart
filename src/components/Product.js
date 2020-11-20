2e000b5e890117be1c13504f305c9d559680b26d
Mark Hillin <mark.hillin@gmail.com>
2020-11-19 18:21:21 -0600

    MVP / Stretch Completed

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './Product.css'
 
-const Product = props => {
+const Product = (props) => {
 	return (
 		<div className="product">
 			<img src={props.product.image} alt={`${props.product.title} book`} />
